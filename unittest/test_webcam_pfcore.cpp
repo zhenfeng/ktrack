d0739672396b8af6e48c1aa03c88ea7d0c229524
peter karasev <karasevpa@gmail.com>
2011-05-14 14:40:50 -0400

    updating tracker demo

--- unittest/test_webcam_pfcore.cpp
+++ unittest/test_webcam_pfcore.cpp
@@ -12,13 +12,31 @@ string window_name = "SimpleMotionTracker";
 class SimpleMotionTracker : public TrackingProblem
 {
   virtual void evaluateLikelihood(const vector<cv::Mat> &particles,
-                                  vector<double> &likelihood) {
-
+                                  vector<double> &likelihood)
+  {
+    // p(z_k | \hat{x}_k )
+    // good particles => measurement is solid
   }
 
   virtual void propagateDynamics(const vector<cv::Mat> &particles_in,
-                                 vector<cv::Mat> &particles_out) {
+                                 vector<cv::Mat> &particles_out)
+  {
 
+  }
+
+  virtual void registerCurrentData(const std::vector<cv::Mat> &images,
+                                   void *extra_data)
+  {
+    image_data = images;
+
+    image_data[0].copyTo(Icurr);
+    cv::GaussianBlur(Icurr,Icurr_smooth,Size(5,5),2.0,2.0);
+
+    if( Iprev.empty() ) {
+      dIdt = 0 * Icurr;
+    } else {
+      cv::absdiff(Icurr_smooth,Iprev_smooth,dIdt);
+    }
   }
 
   virtual size_t N() const {
@@ -28,9 +46,36 @@ class SimpleMotionTracker : public TrackingProblem
   virtual void outputCallback(const vector<cv::Mat> &particles,
                               const vector<double> &weights)
   {
-    cv::imshow(window_name,image_data[0]);
+    // Assign the "low dimensional outputs"
+
+    track_box.x      = Icurr.cols / 2;
+    track_box.y      = Icurr.rows / 2;
+    track_box.width  = 20;
+    track_box.height = 20;
+
+    Icurr.copyTo(Iprev);
+    Icurr_smooth.copyTo(Iprev_smooth);
   }
 
+  // **** Functions specific to this incarnation of TrackingProblem
+public:
+  /** draw the track_box on top of an image, for display */
+  void drawTrackBox( cv::Mat& outimg ) {
+    dIdt.copyTo(outimg);
+
+    cv::rectangle(outimg, track_box, Scalar(255,255,255),3,CV_AA);
+  }
+
+  // **** Data specific to this incarnation of TrackingProblem
+
+private:
+  Mat dIdt;
+  Mat Icurr;
+  Mat Iprev;
+  Mat Iprev_smooth;
+  Mat Icurr_smooth;
+
+  cv::Rect  track_box;
 
 };
 
@@ -65,7 +110,11 @@ int main( int argc, char* argv [] )
       continue;
 
     image_container[0] = frame;
+    image_container[1] = frame;
     pft.processNewData(image_container, meta_data );
+
+    prob->drawTrackBox(image_container[1]);
+    cv::imshow(window_name,image_container[1]);
 
     //cv::Point2f pt_xy;
     //cout << "tracking at:  x = " << pt_xy.x << ", y = " << pt_xy.y << endl;
