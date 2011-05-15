e12d82b7ca39da2623a5f57c2774adc381587e8e
peter karasev <karasevpa@gmail.com>
2011-05-14 20:25:41 -0400

    add file to run on dir of images

---
+++ include/file_io.h
@@ -0,0 +1,25 @@
+/*
+ * file_io.cpp
+ *
+ *  Created on: Nov 27, 2010
+ *      Author: ethan
+ */
+
+#ifndef FILE_IO_CPP_ROBOTVIEW_JNI
+#define FILE_IO_CPP_ROBOTVIEW_JNI
+
+
+
+#include <string>
+#include <vector>
+
+void getFilePrefixes(const char * dir, const std::string& extension,
+                     std:: vector<std::string>& files);
+int getFileNum(const char * dir, const std::string& extension);
+
+void lsFilesOfType(const char * dir, const std::string& extension,
+                   std::vector<std::string>& files);
+
+
+
+#endif /* FILE_IO_CPP_ */
---
+++ src/file_io.cpp
@@ -0,0 +1,71 @@
+/*
+ * file_io.cpp
+ *
+ *  Created on: Nov 27, 2010
+ *      Author: ethan
+ */
+
+#include "file_io.h"
+#include <sys/types.h>
+#include <dirent.h>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+void lsFilesOfType(const char * dir, const string& extension,
+        vector<string>& files) {
+    files.clear();
+    DIR *dp;
+    struct dirent *dirp;
+    if ((dp = opendir(dir)) == NULL) {
+        return;
+    }
+
+    while ((dirp = readdir(dp)) != NULL) {
+        std::string name(dirp->d_name);
+        size_t pos = name.find(extension);
+        if (pos != std::string::npos) {
+            files.push_back(name);
+        }
+    }
+    closedir(dp);
+}
+
+void getFilePrefixes(const char * dir, const string& extension,
+        vector<string>& files) {
+
+    files.clear();
+    DIR *dp;
+    struct dirent *dirp;
+    if ((dp = opendir(dir)) == NULL) {
+        return;
+    }
+
+    while ((dirp = readdir(dp)) != NULL) {
+        std::string name(dirp->d_name);
+        size_t pos = name.find(extension);
+        if (pos != std::string::npos) {
+            files.push_back(name.substr(0, pos));
+        }
+    }
+    closedir(dp);
+}
+int getFileNum(const char * dir, const std::string& extension) {
+    int count = 0;
+    DIR *dp;
+    struct dirent *dirp;
+    if ((dp = opendir(dir)) == NULL) {
+        return 0;
+    }
+
+    while ((dirp = readdir(dp)) != NULL) {
+        std::string name(dirp->d_name);
+        size_t pos = name.find(extension);
+        if (pos != std::string::npos) {
+            count++;
+        }
+    }
+    closedir(dp);
+    return count;
+}
